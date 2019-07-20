11310945b3e5a52ddb98d732e36cfc2567994b34
326840255 <326840255@qq.com>
2019-07-20 12:02:00 +0800

    数据库配置支持读写分离,在不支持config 修改数组配置

--- config/database.php
+++ config/database.php
@@ -3,6 +3,12 @@
 return [
 
 	'type'            => 'mysql',
+    'read' => [
+        'hostname' => ['127.0.0.1', '127.0.0.1']
+    ],
+    'write' => [
+        'hostname' => '127.0.0.1'
+    ],
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
@@ -14,7 +20,7 @@ return [
     // 端口
     'hostport'        => '3306',
     //分布式
-    'hadoop'           => true ,
+    'hadoop'           => false ,
     //记录日志
     'log'           => true,
     //记录explain,true 全部 或者超过阀值
